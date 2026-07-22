ae0c083e96eb75236f31afaf89b32805a29474c6
Bom dia! <Samuel Renildo>
2026-07-22 15:12:19 -0300

    chore: remove node_modules e dist do versionamento do github

--- dist/calculadora.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=calculadora.d.ts.map
--- dist/calculadora.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function calcularTotal(preco, quantidade) {
-    return preco * quantidade;
-}
-const total = calcularTotal(49.9, 3);
-console.log(`Total de estoque: R$ ${total.toFixed(2)}`);
-//# sourceMappingURL=calculadora.js.map
